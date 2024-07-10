e79367304c6169c27e9cb3810d4bf3ea439c498c
Silf2 <sylvain.guenet1@gmail.com>
2024-07-10 13:48:09 +0200

    (Update) Update on basic pages

--- src/Controller/Users/RegisterUser.php
+++ src/Controller/Users/RegisterUser.php
@@ -20,9 +20,7 @@ final class RegisterUser
 {
     public function __construct(
         private Environment $twig,
-        //private FormFactoryInterface $formFactory,
-        //private UserPasswordHasherInterface $passwordHasher,
-        //private EntityManagerInterface $entityManager,
+        private FormFactoryInterface $formFactory,
         private UrlGeneratorInterface $urlGenerator
     )
     {}
@@ -30,26 +28,18 @@ final class RegisterUser
     #[Route('/register', name: "app_register")]
     public function __invoke(Request $request): Response
     {
-        /*$user = new User();
-        $user->setRoles(['ROLE_USER']);
-
-        $form = $this->formFactory->create(UserRegisterType::class, $user);
+        $form = $this->formFactory->create(UserRegisterType::class);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $password = $user->getPassword();
-            $user->setPassword($this->passwordHasher->hashPassword($user, $password));
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
 
             $url = $this->urlGenerator->generate('app_home');
             return new RedirectResponse($url);
-        }*/
+        }
 
-        $content = $this->twig->render("users/registerPage.html.twig"/*, [
+        $content = $this->twig->render("users/registerPage.html.twig", [
             'form' => $form->createView()
-        ]*/);
+        ]);
         return new Response($content);
     }
 }
